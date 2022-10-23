57af1328ca99e5305357880630a24acc0005ea66
Ryan Wang <i@ryanc.cc>
2022-10-23 21:43:54 +0800

    feat: add setup dom selector support

--- src/main/java/run/halo/lightgallery/LightGalleryHeadProcessor.java
+++ src/main/java/run/halo/lightgallery/LightGalleryHeadProcessor.java
@@ -1,11 +1,13 @@
 package run.halo.lightgallery;
 
+import lombok.Data;
 import org.springframework.stereotype.Component;
 import org.thymeleaf.context.ITemplateContext;
 import org.thymeleaf.model.IModel;
 import org.thymeleaf.model.IModelFactory;
 import org.thymeleaf.processor.element.IElementModelStructureHandler;
 import reactor.core.publisher.Mono;
+import run.halo.app.plugin.SettingFetcher;
 import run.halo.app.theme.dialect.TemplateHeadProcessor;
 
 /**
@@ -14,45 +16,47 @@ import run.halo.app.theme.dialect.TemplateHeadProcessor;
 @Component
 public class LightGalleryHeadProcessor implements TemplateHeadProcessor {
 
-    public LightGalleryHeadProcessor() {
+    private final SettingFetcher settingFetcher;
+
+    public LightGalleryHeadProcessor(SettingFetcher settingFetcher) {
+        this.settingFetcher = settingFetcher;
     }
 
     @Override
     public Mono<Void> process(ITemplateContext context, IModel model,
                               IElementModelStructureHandler structureHandler) {
-        return Mono.just(context.getTemplateData().getTemplate())
-                .filter(this::isContentTemplate)
-                .map(s -> {
-                    IModelFactory modelFactory = context.getModelFactory();
-                    model.add(modelFactory.createText(lightGalleryScript()));
+        return settingFetcher.fetch("basic", BasicConfig.class)
+                .map(basicConfig -> {
+                    final IModelFactory modelFactory = context.getModelFactory();
+                    model.add(modelFactory.createText(lightGalleryScript(basicConfig.getDom_selector())));
                     return Mono.empty();
-                })
-                .then();
+                }).orElse(Mono.empty()).then();
     }
 
-    private String lightGalleryScript() {
+    private String lightGalleryScript(String domSelector) {
         return """
                 <!-- PluginLightGallery start -->
                 <link href="/plugins/PluginLightGallery/assets/static/css/lightgallery.min.css" rel="stylesheet" />
                 <script src="/plugins/PluginLightGallery/assets/static/js/lightgallery.min.js"></script>
                 <script type="text/javascript">
                     document.addEventListener("DOMContentLoaded", function () {
-                      const imageNodes = document.querySelectorAll("#content img");
+                      const imageNodes = document.querySelectorAll(`%s img`);
                       imageNodes.forEach(function (node) {
                         if (node) {
                           node.dataset.src = node.src;
                         }
                       });
-                      lightGallery(document.getElementById("content"), {
+                      lightGallery(document.querySelector("%s"), {
                         selector: "img",
                       });
                     });
                 </script>
                 <!-- PluginLightGallery end -->
-                """;
+                """.formatted(domSelector, domSelector);
     }
 
-    private boolean isContentTemplate(String template) {
-        return "post".equals(template) || "page".equals(template);
+    @Data
+    public static class BasicConfig {
+        String dom_selector;
     }
 }
