832c8c0bc716ea0d69fb608d00ce70c53e7b3d19
guqing <i@guqing.email>
2024-07-02 17:43:38 +0800

    chore: remove deprecated plugin wrapper

--- src/main/java/run/halo/lightgallery/LightGalleryPlugin.java
+++ src/main/java/run/halo/lightgallery/LightGalleryPlugin.java
@@ -1,8 +1,8 @@
 package run.halo.lightgallery;
 
-import org.pf4j.PluginWrapper;
 import org.springframework.stereotype.Component;
 import run.halo.app.plugin.BasePlugin;
+import run.halo.app.plugin.PluginContext;
 
 /**
  * @author ryanwang
@@ -10,8 +10,8 @@ import run.halo.app.plugin.BasePlugin;
 @Component
 public class LightGalleryPlugin extends BasePlugin {
 
-    public LightGalleryPlugin(PluginWrapper wrapper) {
-        super(wrapper);
+    public LightGalleryPlugin(PluginContext pluginContext) {
+        super(pluginContext);
     }
 
     @Override
